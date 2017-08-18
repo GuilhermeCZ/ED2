d45ba05a98cdd44ecb609ac82af7ca897b16edc1
GuilhermeCZ <guilhermecaonzambonin@hotmail.com>
2017-08-18 19:09:20 -0300

    Binary Tree
    Esqueleto da Arvore binaria

---
+++ bstree.cpp
@@ -0,0 +1,29 @@
+//--- Árvore Binária ---//
+#include <stdio.h>
+#include <stdlib.h>
+
+node {
+  int key;
+  node *left, *rigth;
+}
+
+//--- Inserir ---//
+node *insert(node *k, int key)  {
+
+}
+
+//--- Deletar ---//
+
+
+//--- Procurar ---//
+
+
+//--- Ordenar ---//
+
+int main() {
+  node *k = NULL;
+
+  
+
+  return 0;
+}
